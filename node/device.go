59029566daa433be552ac326040dc092cb241c84
isaac <isaacgrowell@gmail.com>
2021-06-25 05:47:48 -0400

    Add second control interface to device

--- node/device.go
+++ node/device.go
@@ -4,11 +4,16 @@ func (d *Device) BuildResource(n Node, index int) {
 	label := getResourceLabel("TestDevice", index)
 	d.BaseResource = SetBaseResourceProperties(label, "NMOS Test Device")
 	d.NodeId = n.ID
-	c := Controls{
+	c1 := Controls{
 		n.Href,
 		"urn:x-nmos:control:sr-ctrl/v1.0",
 		false,
 	}
+	c2 := Controls{
+		"http://172.16.220.169:4003",
+		"urn:x-nmos:control:sr-ctrl/v1.0",
+		false,
+	}
 	d.Type = "urn:x-nmos:device:generic"
-	d.Controls = append(d.Controls, c)
+	d.Controls = append(d.Controls, c1, c2)
 }
