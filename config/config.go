0a675b80e7248ab0617063271c8d6fc0b836c8b7
isaacgr <isaacgrowell@gmail.com>
2024-08-27 08:57:39 -0400

    Allow reading in config from stdin

--- config/config.go
+++ config/config.go
@@ -15,28 +15,63 @@ var once sync.Once
 
 func New() *Config {
 	once.Do(func() {
-		config = parseConfig()
+		config = loadConfig()
 	})
 	return config
 }
 
-func parseConfig() *Config {
+//func parseConfig() *Config {
+//	flag.Parse()
+//	c := &Config{}
+//	configFile, err := os.Open(*ConfigFilename)
+//	if err != nil {
+//		log.Fatal(err)
+//		os.Exit(1)
+//	}
+//
+//	byteValue, err := ioutil.ReadAll(configFile)
+//	if err != nil {
+//		log.Fatal(err)
+//		os.Exit(1)
+//	}
+//	err = json.Unmarshal(byteValue, &c)
+//	if err != nil {
+//		log.Fatal("Unable to parse config file")
+//	}
+//	return c
+//}
+
+func loadConfig() *Config {
 	flag.Parse()
 	c := &Config{}
-	configFile, err := os.Open(*ConfigFilename)
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
+    var configData []byte
+    var err error
 
-	byteValue, err := ioutil.ReadAll(configFile)
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-	err = json.Unmarshal(byteValue, &c)
-	if err != nil {
-		log.Fatal("Unable to parse config file")
-	}
-	return c
+    if *ConfigFilename == "-" {
+        // Read from standard input
+        configData, err = ioutil.ReadAll(os.Stdin)
+        if err != nil {
+            log.Fatal(err)
+            os.Exit(1)
+        }
+    } else {
+        // Read from file
+        configFile, err := os.Open(*ConfigFilename)
+        if err != nil {
+            log.Fatal(err)
+            os.Exit(1)
+        }
+        configData, err = ioutil.ReadAll(configFile)
+        if err != nil {
+            log.Fatal(err)
+            os.Exit(1)
+        }
+    }
+
+    //var config Config
+    if err := json.Unmarshal(configData, &c); err != nil {
+        log.Fatal("Unable to parse config file")
+    }
+
+    return c 
 }
