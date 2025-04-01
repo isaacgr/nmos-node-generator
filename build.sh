8013cb0bbba71d9a6f498c062c699fc49106505b
irowell <irowell@evertz.com>
2025-04-01 08:51:08 -0400

    Update build script

--- build.sh
+++ build.sh
@@ -2,9 +2,5 @@
 
 go build .
 cp nmos-node-generator /mnt/c/Users/irowell/Documents/nmos_node_generator/
-# cp config.json /mnt/c/Users/irowell/Documents/nmos_node_generator/
-# mv nmos-node-generator /var/www/nmos-node-generator/
+cp config.json /mnt/c/Users/irowell/Documents/nmos_node_generator/
 env GOOS=windows GOARCH=amd64 go build .
-# mv nmos-node-generator.exe /var/www/nmos-node-generator/
-# cp README.md /var/www/nmos-node-generator/
-# cp config.json /var/www/nmos-node-generator/
